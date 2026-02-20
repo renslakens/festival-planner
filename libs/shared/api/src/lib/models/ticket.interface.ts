a444bf3cfd7a106342e80da11e9a555c5546e1aa
Rens Lakens <rens@lakens.org>
2026-02-20 13:27:09 +0100

    fix: purchasedate and userid not in ticketcreate function

--- libs/shared/api/src/lib/models/ticket.interface.ts
+++ libs/shared/api/src/lib/models/ticket.interface.ts
@@ -3,18 +3,14 @@ import { Id } from './id.type';
 export interface ITicket {
     _id: Id;
     name: string;
-    purchaseDate: Date;
+    purchaseDate?: Date;
     price: number;
     festivalId: Id;
-    userId: Id;
+    userId?: Id;
 }
 
 export type ICreateTicket = Pick<
     ITicket,
-    'name' | 'purchaseDate' | 'price' | 'festivalId' | 'userId'
+    'name' | 'price' | 'festivalId'
 >;
 export type IUpdateTicket = Partial<Omit<ITicket, 'id'>>;
-
-// export interface ITicketResponse {
-//     results: ITicket;
-// }
