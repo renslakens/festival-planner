df4556e01417dd6c23570a553a275c8f1aa8ea80
Rens Lakens <rens@lakens.org>
2026-03-01 22:47:57 +0100

    fix: /api global prefix added

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
     app.enableCors();
     if (process.env.NODE_ENV !== 'production') {
         await app.listen(3000);
