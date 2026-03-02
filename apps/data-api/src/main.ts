85306beb0bcf203c5f1b8ea5714562b684572aad
Rens Lakens <rens@lakens.org>
2026-03-02 13:07:05 +0100

    fix: added port env to main.ts data-api

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    const port = 3000;
+    const port = process.env.PORT || 3000;
 
     app.enableCors();
     await app.listen(port, '0.0.0.0');
