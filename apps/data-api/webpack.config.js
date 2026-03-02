030aa22f60b6d553fa17624b47651cceb90ff399
Rens Lakens <rens@lakens.org>
2026-03-02 16:30:31 +0100

    fix: webpack build data-api

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,20 +1,27 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
-module.exports = {
-  output: {
-    path: join(__dirname, '../../dist/apps/data-api'),
-  },
-  plugins: [
-    new NxAppWebpackPlugin({
-      target: 'node',
-      compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ['./src/assets'],
-      optimization: false,
-      outputHashing: 'none',
-      generatePackageJson: true,
-    }),
-  ],
-};
+const { composePlugins, withNx } = require('@nx/webpack');
+
+module.exports = composePlugins(withNx(), (config) => {
+  // The output will be a single file named main.js
+  return config;
+});
+
+// module.exports = {
+//   output: {
+//     path: join(__dirname, '../../dist/apps/data-api'),
+//   },
+//   plugins: [
+//     new NxAppWebpackPlugin({
+//       target: 'node',
+//       compiler: 'tsc',
+//       main: './src/main.ts',
+//       tsConfig: './tsconfig.app.json',
+//       assets: ['./src/assets'],
+//       optimization: false,
+//       outputHashing: 'none',
+//       generatePackageJson: true,
+//     }),
+//   ],
+// };
