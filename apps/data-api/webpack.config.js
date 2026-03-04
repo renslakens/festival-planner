cef03cd428c9ab3291466cfe3974c0fd1f2d7782
Rens Lakens <rens@lakens.org>
2026-03-04 15:25:45 +0100

    fix: baseUrl fix 1

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,15 +1,12 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 
 module.exports = composePlugins(withNx(), (config) => {
-  config.externals = [
-    ...(config.externals || []),
-    // Ignore optional NestJS dependencies that cause bundle errors
-    '@nestjs/microservices',
-    '@nestjs/websockets/socket-module',
-    '@nestjs/microservices/microservices-module',
-    'cache-manager',
-    'class-transformer',
-    'class-validator'
+  // Handle NestJS lazy-loading warnings
+  config.ignoreWarnings = [
+    { module: /@nestjs\/common/ },
+    { module: /@nestjs\/core/ },
+    { module: /express/ },
+    { module: /mongodb/ }
   ];
 
   return config;
