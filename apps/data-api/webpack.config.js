c1c1ace00ff56d072ea127cb4bb341cfe91c4fc8
Rens Lakens <rens@lakens.org>
2026-03-04 15:21:31 +0100

    fix: fixed deploy settings

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -1,10 +1,16 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
-const { join } = require('path');
-
 const { composePlugins, withNx } = require('@nx/webpack');
 
 module.exports = composePlugins(withNx(), (config) => {
-  config.externals = [];
+  config.externals = [
+    ...(config.externals || []),
+    // Ignore optional NestJS dependencies that cause bundle errors
+    '@nestjs/microservices',
+    '@nestjs/websockets/socket-module',
+    '@nestjs/microservices/microservices-module',
+    'cache-manager',
+    'class-transformer',
+    'class-validator'
+  ];
 
   return config;
 });
