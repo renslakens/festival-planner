c1b6dfa1b10620ada72b02e96f9daf50f408535b
Rens Lakens <rens@lakens.org>
2026-03-04 15:11:10 +0100

    fix: webpack external plugin

--- apps/data-api/webpack.config.js
+++ apps/data-api/webpack.config.js
@@ -4,24 +4,7 @@ const { join } = require('path');
 const { composePlugins, withNx } = require('@nx/webpack');
 
 module.exports = composePlugins(withNx(), (config) => {
-  // The output will be a single file named main.js
+  config.externals = [];
+
   return config;
 });
-
-// module.exports = {
-//   output: {
-//     path: join(__dirname, '../../dist/apps/data-api'),
-//   },
-//   plugins: [
-//     new NxAppWebpackPlugin({
-//       target: 'node',
-//       compiler: 'tsc',
-//       main: './src/main.ts',
-//       tsConfig: './tsconfig.app.json',
-//       assets: ['./src/assets'],
-//       optimization: false,
-//       outputHashing: 'none',
-//       generatePackageJson: true,
-//     }),
-//   ],
-// };
